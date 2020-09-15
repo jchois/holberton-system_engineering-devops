c0c95aec04581ca0e3fc610480524856387a0221
jchois <choisjuliana@gmail.com>
2020-09-14 22:33:58 -0500

    Fixing

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -6,13 +6,11 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    # get url
-    users = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                         .format(argv[1]))
-    todo = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
-                        .format(argv[1]))
+    # get url by id:
+    user = requests.get('https://jsonplaceholder.typicode.com/users/')
+    user_list = user.json()
 
-    data_dict = {}
+    json_dict = {}
 
     for u_name in user_list:
         todo = requests.get(
@@ -20,7 +18,7 @@ if __name__ == "__main__":
             .format(u_name['id']))
         todo_list = todo.json()
 
-        data_list = []
+        json_list = []
 
         # create a new dict with the user items
         for data in todo_list:
@@ -28,13 +26,12 @@ if __name__ == "__main__":
             user_task["username"] = u_name['username']
             user_task["task"] = data['title']
             user_task["completed"] = data['completed']
-            data_list.append(user_task)
+            json_list.append(user_task)
 
-        data_dict[u_name['id']] = data_list
+        json_dict[u_name['id']] = json_list
 
     # json file name
     json_file = 'todo_all_employees.json'
-
     # insert items in file
     with open(json_file, mode='w') as jfile:
-        json.dump(data_dict, jfile)
+        json.dump(json_dict, jfile)

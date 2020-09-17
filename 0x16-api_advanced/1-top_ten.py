0e18001239bc4e42954e0c64f680a1853ff04ae1
jchois <choisjuliana@gmail.com>
2020-09-17 17:28:26 -0500

    Task 1 - Top 10 hot posts

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+prints the titles of the first 10 hot posts listed for a given subreddit
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """comments"""
+    URL = 'http://reddit.com/r/{}/hot.json'
+    headers = {"User-Agent": "jchois"}
+
+    response = requests.get(URL.format(subreddit), params={
+                            'limit': 10}, headers=headers)
+
+    posts = response.json().get('data', {}).get('children', None)
+
+    if posts:
+        for titles in posts:
+            print(titles.get('data').get('title'))
+    else:
+        print('None')
